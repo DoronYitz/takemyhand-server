35a9269032d043015c3a5a5cac11064c76e4326e
DoronYitz <73228405+DoronYitz@users.noreply.github.com>
2021-09-22 14:58:00 +0300

    jwt added

--- src/routes/parcel.route.ts
+++ src/routes/parcel.route.ts
@@ -1,21 +1,39 @@
 import { Router } from "express";
-import { createParcel, deleteParcel, editParcel, getParcel, getParcels } from "../controllers/parcel.controller";
+import {
+	createParcel,
+	deleteParcel,
+	editParcel,
+	getParcel,
+	getParcels,
+} from "../controllers/parcel.controller";
 
 const router = Router();
 
-// Find all
+/**
+ * @route   GET api/parcel
+ * @desc    Get all parcels
+ * @access  Private
+ */
 router.get("/", getParcels);
 
-// Find one
+// @route   GET api/parcel/<id>
+// @desc    Get parcel by id
+// @access  Private
 router.get("/:id", getParcel, (req, res) => res.json(req.parcel));
 
-// Create
+// @route   POST api/parcel
+// @desc    Create parcel
+// @access  Private
 router.post("/", createParcel);
 
-// Edit
+// @route   PATCH api/parcel/<id>
+// @desc    Edit parcel
+// @access  Private
 router.patch("/:id", getParcel, editParcel);
 
-// Delete
+// @route   DELETE api/parcel/<id>
+// @desc    Delete parcel
+// @access  Admin
 router.delete("/:id", getParcel, deleteParcel);
 
 export default router;
